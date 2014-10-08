8eb9b0edc8725ea08c0f296cff3e9cbb3660b6d3
monofuel <monofuel@japura.net>
2014-10-07 21:39:42 -0400

    filled in functions for basic functionality

--- src/test.c
+++ src/test.c
@@ -1,9 +1,14 @@
 #include "test.h"
 
+//ugly global variables!
+float a,b,c,disc,x1,x2;
+
 int input() {
+	scanf("%f %f %f",&a,&b,&c);
 	return 0;
 }
 int output() {
+	printf("roots are %f and %f\n",x1,x2);
 	return 0;
 }
 float discriminant(float a, float b, float c) {
@@ -11,5 +16,14 @@ float discriminant(float a, float b, float c) {
 }
 
 int main() {
+	input();
+	disc = discriminant(a,b,c);
+	if (DEBUG) printf("the discriminant is %f\n",disc);
+
+	x1 = (-b + sqrt(disc))/(2*a);
+  x2 = (-b - sqrt(disc))/(2*a);
+
+  output();
+
 	return 0;
 }
--- src/test.h
+++ src/test.h
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <math.h>
+#define DEBUG 1
 
 int input();
 int output();
