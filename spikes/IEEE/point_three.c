fe761fa4462056388c5e86ba958604808ab20e7f
monofuel <monofuel@japura.net>
2014-10-19 15:31:58 -0400

    improved printed precision for spike

--- spikes/IEEE/point_three.c
+++ spikes/IEEE/point_three.c
@@ -9,7 +9,7 @@ int main() {
 	if (i == 0.3) {
 		printf("the float of 0.1 + 0.1 + 0.1 is equal to 0.3\n");
 	} else {
-		printf("the float of 0.1 + 0.1 + 0.1 is not equal to 0.3, it is %f\n",i);
+		printf("the float of 0.1 + 0.1 + 0.1 is not equal to 0.3, it is %.10f\n",i);
 	}
 
 	double k = 0;
@@ -18,7 +18,7 @@ int main() {
 	if (k == 0.3) {
 		printf("the double of 0.1 + 0.1 + 0.1 is equal to 0.3\n");
 	} else {
-		printf("the double of 0.1 + 0.1 + 0.1 is not equal to 0.3, it is %lf\n",k);
+		printf("the double of 0.1 + 0.1 + 0.1 is not equal to 0.3, it is %.20lf\n",k);
 	}
 
 	return 0;
