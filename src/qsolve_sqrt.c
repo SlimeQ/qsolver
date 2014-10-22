5463305e5d750080de6dba9566765a09c604ac36
monofuel <monofuel@japura.net>
2014-10-21 22:07:32 -0400

    added jordan's sqrt algorithm

--- src/qsolve_sqrt.c
+++ src/qsolve_sqrt.c
@@ -3,5 +3,17 @@
 
 double qsolve_sqrt(double x){
 
-  return 0;
+  	union
+	{
+		int i;
+		float x;
+	}u;
+
+	u.x = x;
+	u.i = (1<<29) + (u.i >> 1) - (1<<22);
+
+	u.x = u.x + x/u.x;
+	u.x = 0.25f*u.x + x/u.x;
+
+	return u.x;
 }
