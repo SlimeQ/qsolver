ae5cec2d99f3d7d5fc9fc08958ce689b5ba3a343
monofuel <monofuel@japura.net>
2014-10-22 01:57:39 -0400

    added test cases

--- src/t4.c
+++ src/t4.c
@@ -23,48 +23,29 @@ cunit_init();
 
 //test the max value for b
 coefs.a = 1.0;
-coefs.b = sqrt(DBL_MAX*1.001);
+coefs.b = DBL_MAX/2;
 coefs.c = 1.0;
 ret = qsolve_roots(&coefs, &roots);
 assert_eq("ret",ret,-4);
-printf("why is it the wrong return code q_q gonna push anyway\n");
 
-// A good unit test, all is in order
-// (x - x1)*(x - x2) = 0
+//test the max value for a
+x1 = 1.0;
+x2 = 3.3;
+coefs.a = DBL_MAX;
+coefs.b = -x1 + -x2;
+coefs.c = x1*x2;
+ret = qsolve_roots(&coefs, &roots);
+assert_eq("ret",ret,-2);
+
+//test the max value for c
 x1 = 3.1;
 x2 = 3.3;
 coefs.a = 1.0;
 coefs.b = -x1 + -x2;
-coefs.c = x1*x2;
+coefs.c = DBL_MAX;
 ret = qsolve_roots(&coefs, &roots);
-assert_eq("ret",ret,2);
-assert_feqrerr("x1",roots.x1, x2, 10.0*cunit_dmacheps );
-assert_feqrerr("x2",roots.x2, x1, 10.0*cunit_dmacheps );
+assert_eq("ret",ret,-2);
 
-// A good unit test allows for round off!
-// (x - x1)*(x - x2) = 0
-x1 = 3.1;
-x2 = 3.3;
-coefs.a = 1.0;
-coefs.b = -x1 + -x2;
-coefs.c = x1*x2;
-ret = qsolve_roots(&coefs, &roots);
-assert_eq("ret",ret,2);
-assert_feqrerr("x1",roots.x1, x2, 10.0*cunit_dmacheps );
-assert_feqrerr("x2",roots.x2, x1, 10.0*cunit_dmacheps );
 
-// A "good" unit test, need to allow for round off!
-// qsolve_roots() passes this one. ;-)
-// This allows about one base 10 least significant digit of error
-// (x - x1)*(x - x2) = 0
-x1 = 3.1;
-x2 = 3.3;
-coefs.a = 1.0;
-coefs.b = -x1 + -x2;
-coefs.c = x1*x2;
-ret = qsolve_roots(&coefs, &roots);
-assert_eq("ret",ret,2);
-assert_feqrerr("x1",roots.x1, x2, 10.0*cunit_dmacheps );
-assert_feqrerr("x2",roots.x2, x1, 10.0*cunit_dmacheps );
 exit(0);
 }
