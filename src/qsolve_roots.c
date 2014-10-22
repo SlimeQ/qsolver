d8822e045d2255cdf4f9ed8b68e5e9c6fc33e017
monofuel <monofuel@japura.net>
2014-10-21 21:44:57 -0400

    added unit testing to main source

---
+++ src/qsolve_roots.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "qsolve_roots.h"
+#include "qsolve_sqrt.h"
+
+
+int qsolve_roots(Coef *coef, Root *root){
+
+  return 0;
+}
---
+++ src/qsolve_sqrt.c
@@ -0,0 +1,7 @@
+#include <math.h>
+#include "qsolve_sqrt.h"
+
+double qsolve_sqrt(double x){
+
+  return 0;
+}
